10a9bd9146aa71f2534c1d8cb176c9e746257776
Nyx <nyxfor13days@gmail.com>
2022-12-14 16:53:36 +0530

    Added Default Theme Type Declaration

---
+++ @types/styled.d.ts
@@ -0,0 +1,106 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      background: string;
+      onBackground: string;
+      surface: string;
+      onSurface: string;
+      surfaceVariant: string;
+      onSurfaceVariant: string;
+      primary: string;
+      onPrimary: string;
+      primaryContainer: string;
+      onPrimaryContainer: string;
+      secondary: string;
+      onSecondary: string;
+      secondaryContainer: string;
+      onSecondaryContainer: string;
+      tertiary: string;
+      onTertiary: string;
+      tertiaryContainer: string;
+      onTertiaryContainer: string;
+      error: string;
+      onError: string;
+    };
+    spacing: {
+      0: string;
+      1: string;
+      2: string;
+      4: string;
+      8: string;
+      10: string;
+      12: string;
+      14: string;
+      16: string;
+      20: string;
+      24: string;
+      28: string;
+      32: string;
+      36: string;
+      40: string;
+      44: string;
+      48: string;
+      52: string;
+      56: string;
+      60: string;
+      64: string;
+      72: string;
+      80: string;
+      96: string;
+    };
+    screens: {
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      "2xl": string;
+    };
+    typography: {
+      fontFamily: string[];
+      fontSize: {
+        xs: string;
+        sm: string;
+        base: string;
+        lg: string;
+        xl: string;
+        "2xl": string;
+        "3xl": string;
+        "4xl": string;
+        "5xl": string;
+        "6xl": string;
+        "7xl": string;
+        "8xl": string;
+        "9xl": string;
+      };
+      lineHeight: {
+        DEFAULT: number;
+        none: number;
+        tight: number;
+        snug: number;
+        normal: number;
+        relaxed: number;
+        loose: number;
+      };
+      letterSpacing: {
+        DEFAULT: string;
+        tighter: string;
+        tight: string;
+        normal: string;
+        wide: string;
+        wider: string;
+        widest: string;
+      };
+    };
+    borderRadius: {
+      DEFAULT: string;
+      none: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      full: string;
+    };
+  }
+}

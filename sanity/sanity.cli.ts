0ec158073d126b5b30bab046c25e9cdc5d79dbef
Nyx <nyxfor13days@gmail.com>
2022-12-14 14:21:40 +0530

    Formatted Files using prettier

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'hunu6pxy',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export default () => {
-	return <div>App</div>;
+  return <div>App</div>;
 };
